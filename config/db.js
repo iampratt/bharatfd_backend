907d52b16fec0382325cc209d6a557a77b8b40ba
iampratt <pratyush0705@gmail.com>
2025-02-01 00:57:33 +0530

    fix: added error handling for mongoose

--- config/db.js
+++ config/db.js
@@ -4,6 +4,11 @@ const mongoose = require('mongoose');
 // MongoDB Connection
 const MONGO_URL = process.env.MONGO_URL;
 
-mongoose.connect(MONGO_URL).then(() => {
-  console.log('MongoDB Connected');
-});
+mongoose
+  .connect(MONGO_URL)
+  .then(() => {
+    console.log('MongoDB Connected');
+  })
+  .catch((err) => {
+    console.log(err);
+  });
