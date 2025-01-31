65d323cabe2790bb639c930584070b831e993b20
iampratt <pratyush0705@gmail.com>
2025-02-01 00:40:48 +0530

    feat: added google translate service

---
+++ services/google_translate.js
@@ -0,0 +1,14 @@
+const { Translate } = require("@google-cloud/translate").v2;
+const translate = new Translate();
+
+const translateText=async (text, targetLang) {
+  try {
+    let [translation] = await translate.translate(text, targetLang);
+    return translation;
+  } catch (error) {
+    console.error("Translation Error:", error);
+    return text;
+  }
+}
+
+module.exports = { translateText };
