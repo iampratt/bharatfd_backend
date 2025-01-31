8bef82645e298258513bdef7bd4eff039714dd9b
iampratt <pratyush0705@gmail.com>
2025-02-01 00:13:13 +0530

    intial commit

---
+++ index.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const dotenv = require('dotenv').config();
+
+const app = express();
+
+PORT = process.env.PORT;
+
+app.use(express.urlencoded({ extended: true }));
+app.set('view engine', 'ejs');
+
+app.listen(PORT || 5000, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
