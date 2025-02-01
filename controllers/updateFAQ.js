bce07a06ae887be6f20ae1279fd60126042fe329
iampratt <pratyush0705@gmail.com>
2025-02-01 23:45:41 +0530

    feat: added route for update faq by id

---
+++ controllers/updateFAQ.js
@@ -0,0 +1,35 @@
+const FAQ = require('../models/faq');
+const { translateLanguage } = require('../utils/google_translate');
+
+const SUPPORTED_LANGUAGES = ['en', 'hi', 'es', 'fr', 'ja'];
+
+const updateFAQ = async (req, res) => {
+  try {
+    const { question, answer } = req.body;
+    const { id } = req.params;
+    const translations = await Promise.all(
+      SUPPORTED_LANGUAGES.map(async (language) => {
+        const translatedQuestion = await translateLanguage(question, language);
+        const translatedAnswer = await translateLanguage(answer, language);
+        return {
+          lang: language,
+          question: translatedQuestion,
+          answer: translatedAnswer,
+        };
+      })
+    );
+
+    const updatedFAQ = await FAQ.findByIdAndUpdate(id, { translations });
+
+    if (!updatedFAQ) {
+      return res.status(404).json({ message: 'FAQ not found' });
+    }
+
+    res.status(200).json(updatedFAQ);
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({ message: 'Error updating FAQ' });
+  }
+};
+
+module.exports = updateFAQ;
--- routes/faq.js
+++ routes/faq.js
@@ -1,10 +1,12 @@
 const express = require('express');
 const getFAQ = require('../controllers/getFAQ');
 const createFAQ = require('../controllers/createFAQ');
+const updateFAQ = require('../controllers/updateFAQ');
 
 const router = express.Router();
 
 router.get('/faqs', getFAQ);
 router.post('/createFAQ', createFAQ);
+router.post('/updateFAQ/:id', updateFAQ);
 
 module.exports = router;
