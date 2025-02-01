7b101055ee73499d30b02ed4c203ec09de78c062
iampratt <pratyush0705@gmail.com>
2025-02-01 23:46:20 +0530

    feat: added styles to ejs

--- index.js
+++ index.js
@@ -7,12 +7,17 @@ const gemini = require('./config/gemini');
 const app = express();
 
 const faqRoutes = require('./routes/faq');
+const faq = require('./models/faq');
 
 PORT = process.env.PORT;
 
 app.use(express.urlencoded({ extended: true }));
 app.set('view engine', 'ejs');
 
+app.get('/', async (req, res) => {
+  const faqs = await faq.find({});
+  res.render('index', { faqs });
+});
 app.use('/api', faqRoutes);
 
 app.listen(PORT || 5000, () => {
